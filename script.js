8345ed544ee8d7648d3f67df1468a6e9c37a2f1f
LouayHouimli <louayhouimli@engineer.com>
2023-08-16 10:58:48 +0100

    Update

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ function togglemenu(){
     icon.classList.toggle("open");
 }
 
+var app = document.getElementById('app');
 
+var typewriter = new Typewriter(app, {
+    loop: true
+});
+
+typewriter.typeString("<strong>I'M LOUAY HOUIMLI</strong>")
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('A 17 YEARS OLD')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('<strong>Front End Developer</strong>')
+    .pauseFor(2500)
+    .start();
