b60fd990ad6d76e1f518fcbb75fc823e4261dc39
LouayHouimli <louayhouimli@engineer.com>
2023-08-18 12:37:41 +0100

    add

--- script.js
+++ script.js
@@ -20,3 +20,25 @@ typewriter.typeString("<strong>I'M LOUAY HOUIMLI</strong>")
     .typeString('<strong>Front End Developer</strong>')
     .pauseFor(2500)
     .start();
+
+// Add this at the end of your existing script.js file
+
+// Add this at the end of your existing script.js file
+
+function toggleDarkMode() {
+  const body = document.body;
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    body.classList.remove("background-animation");
+  } else {
+    body.classList.add("background-animation");
+  }
+}
+
+
+
+
+
+
+
