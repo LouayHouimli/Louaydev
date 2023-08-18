0b287231ab3334503a4577612b6f28214300016b
LouayHouimli <louayhouimli@engineer.com>
2023-08-18 10:38:33 +0100

    update

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function togglemenu(){
-    const menu = document.querySelector(".menu-links");
-    const icon = document.querySelector(".hamburger-icon");
-    menu.classList.toggle("open");
-    icon.classList.toggle("open");
+function toggleMenu() {
+  const menu = document.querySelector(".menu-links");
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
 
 var app = document.getElementById('app');
