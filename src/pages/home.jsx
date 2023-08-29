dd796e3a126b15371a2127d012b027e967d7b8e7
LouayHouimli <louayhouimli@engineer.com>
2023-08-29 14:51:29 +0100

    Create home.jsx

---
+++ src/pages/home.jsx
@@ -0,0 +1,22 @@
+// pages/index.js
+import React from 'react';
+import HomeNav from '../components/Nav';
+import HomeHamburgerNav from '../components/HamburgerNav';
+import HomeSectionProfile from '../components/SectionProfile';
+import HomeSectionAbout from '../components/SectionAbout';
+import HomeSectionExperience from '../components/SectionExperience';
+import Footer from '../components/Footer';
+
+const Home = () => (
+  <>
+    <HomeNav />
+    <HomeHamburgerNav />
+    <HomeSectionProfile />
+    <HomeSectionAbout />
+    <HomeSectionExperience />
+    {/* Other sections */}
+    <Footer />
+  </>
+);
+
+export default Home;
