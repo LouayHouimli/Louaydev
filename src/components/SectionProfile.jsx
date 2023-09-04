cbb9155b0c94e60664e1ca79f52b8919e3424f9d
LouayHouimli <louayhouimli@engineer.com>
2023-09-04 10:58:04 +0100

    update Disccord

--- src/components/SectionProfile.jsx
+++ src/components/SectionProfile.jsx
@@ -15,6 +15,27 @@ const SectionProfile = () => {
     }
   };
 
+  const discordText = 'Louay#6287';
+
+  const copyToClipboard = () => {
+    // Create a temporary input element
+    const tempInput = document.createElement('input');
+    tempInput.value = discordText;
+    document.body.appendChild(tempInput);
+
+    // Select the text in the input element
+    tempInput.select();
+
+    // Execute the copy command
+    document.execCommand('copy');
+
+    // Remove the temporary input element
+    document.body.removeChild(tempInput);
+
+    // Show a prompt indicating that the text has been copied
+    window.alert('Text copied: ' + discordText);
+  };
+
   return (
     <section id="profile">
       <div className="section__pic-container">
@@ -24,7 +45,7 @@ const SectionProfile = () => {
         <div id="app">
           <Typewriter
             options={{
-              strings: ["<strong>I'M LOUAY HOUIMLI </strong>", 'A 17 YEARS OLD',"<strong>Front End Developer</strong>"],
+              strings: ["<strong>I'M LOUAY HOUIMLI </strong>", 'A 17 YEARS OLD', "<strong>Front End Developer</strong>"],
               autoStart: true,
               loop: true,
             }}
@@ -43,18 +64,24 @@ const SectionProfile = () => {
             src={linkedinimage}
             alt="My LinkedIn profile"
             className="icon"
-            id='icons'
+            id="icons"
             onClick={() => window.location.href = '#'}
           />
           <img
             src={githubimage}
             alt="My Github profile"
             className="icon"
-            id='icons'
+            id="icons"
             onClick={() => window.open('https://github.com/LouayHouimli')}
           />
-          <img src={discordimage} alt="Louay discord" className="icon" id='icons' />
-          <img src={facebookimage} alt="Louay facebook" className="icon" id='icons' />
+          <img
+            src={discordimage}
+            alt="Louay discord"
+            className="icon"
+            id="icons"
+            onClick={copyToClipboard} // Add the onClick event handler for Discord
+          />
+          <img src={facebookimage} alt="Louay facebook" className="icon" id="icons" />
           <img src={instagramimage} alt="Louay instagram" className='icon' id='icons' />
           <img src={twitterimage} alt="Louay twitter" className='icon' id='icons' />
         </div>
